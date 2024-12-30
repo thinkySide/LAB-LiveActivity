b8508704ff279557d88a312b54fee1fdd9dca313
thinkyside <eunlyuing@gmail.com>
2024-12-30 14:09:26 +0900

    Initial Xcode Project

---
+++ LAB-LiveActivity/LAB-LiveActivity/LAB_LiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  LAB_LiveActivityApp.swift
+//  LAB-LiveActivity
+//
+//  Created by 김민준 on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LAB_LiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
