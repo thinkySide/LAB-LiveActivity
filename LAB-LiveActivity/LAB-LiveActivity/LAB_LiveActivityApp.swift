7a6b010e5fbccd337bfb6e192f707f0ff60dd298
thinkyside <eunlyuing@gmail.com>
2024-12-30 14:14:59 +0900

    MainView Setting

--- LAB-LiveActivity/LAB-LiveActivity/LAB_LiveActivityApp.swift
+++ LAB-LiveActivity/LAB-LiveActivity/LAB_LiveActivityApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LAB_LiveActivityApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
---
+++ LAB-LiveActivity/LAB-LiveActivity/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  LAB-LiveActivity
+//
+//  Created by 김민준 on 12/30/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("LAB-LiveActivity")
+            .font(.system(size: 32, weight: .bold))
+    }
+}
+
+#Preview {
+    MainView()
+}
